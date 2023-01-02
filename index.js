b3323864fc7f29efbab612deab6cc15023940691
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2023-01-02 10:43:07 +0300

    Got JS to load CSS animation

--- index.js
+++ index.js
@@ -3,4 +3,13 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>Flex Panel Gallery</h1>`;
+appDiv.innerHTML = `<h1>Flex Panel Gallery - click a word, please.</h1>`;
+
+//JS to add and remove class of open to start CSS animation
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen(){
+  this.classList.toggle('open');
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
